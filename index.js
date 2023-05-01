30129b1b525e0b29a301c5bb90c8287634860d01
TJ Momon <tjmomon92@gmail.com>
2023-05-01 18:49:17 -0500

    created array of prompt questions regarding manager

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const Manager = require('./Employee-Build/Manager-Build');
+const Engineer = require('./Employee-Build/Engineer-Build');
+const Intern = require('./Employee-Build/Intern-Build');
+
+//empty array for employees
+let teamList = [];
+
+//adds a manager via prompt, then adds to the array
+function addManager() {
+    inquirer.prompt( [
+        {
+            name: 'name',
+            type: 'input',
+            message: 'Manager name?'
+        },
+        {
+            name: 'id',
+            type: 'input',
+            message: 'Manager employee ID?'
+        },
+        {
+            name: 'email',
+            type: 'input',
+            message: 'Manager Email?'
+        },
+        {
+            name: 'officeNumber',
+            type: 'input',
+            message: 'Manager office number?'
+        } 
+    ]).then ( (manager) => {
+        teamList.push(new Manager(manager.name, manager.id, manager.email, manager.officeNumber))
+        employeeRole()
+    })
+};
