9e5cbb9a1b293f0e979820ff52fcbd1bfb31506b
TJ Momon <tjmomon92@gmail.com>
2023-05-01 19:16:22 -0500

    Added Engineer role function, an employee role function, and the more employees function for testing

--- index.js
+++ index.js
@@ -35,3 +35,83 @@ function addManager() {
         employeeRole()
     })
 };
+
+//asks user if engineer or intern is needed; calls addEngineer/ addIntern function
+function employeeRole() {
+    inquirer.prompt( [
+        {
+            name: 'employeeType',
+            type: 'list',
+            message: 'Add an Engineer or Intern?',
+            choices: [
+                'Engineer',
+                'Intern'
+            ]
+        }
+    ]).then ((response) => {
+        if (response.employeeType === 'Engineer') {
+            addEngineer();
+        }
+        // if (response.employeeType === 'Intern') {
+        //     addIntern();
+        // }
+    })
+};
+
+//adds an engineer via prompt, then adds to the array
+function addEngineer() {
+    inquirer.prompt ( [ 
+        {
+            name: 'name',
+            type: 'input',
+            message: 'Engineer name?'
+        },
+        {
+            name: 'id',
+            type: 'input',
+            message: 'Engineer employee ID?'
+        },
+        {
+            name: 'email',
+            type: 'input',
+            message: 'Engineer Email?'
+        },
+        {
+            name: 'github',
+            type: 'input',
+            message: "Engineer GitHub?"
+        }
+    ]).then((response) => {
+        teamList.push(new Engineer(response.name, response.id, response.email, response.github))
+        moreEmployees();
+    })
+};
+
+//adds an intern via prompt, then adds to the array
+function addIntern () {
+
+}
+
+//asks user if more employees are needed; if yes, calls add employees function, if no, writes the HTML
+function moreEmployees() {
+    inquirer.prompt( [
+        {
+            name: 'addMore',
+            type: 'list',
+            message: 'Add more employees?',
+            choices: [
+                'yes',
+                'no'
+            ]
+        }
+    ]).then((response) => {
+        if (response.addMore === 'yes') {
+            employeeRole();
+        }
+        if (response.addMore === 'no') {
+            fs.writeFile('./browser/index.html', build.buildHTML(build.buildCards(teamList)), (err) => {
+                err ? console.error(err) : console.log('The HTML has been built')
+            })
+        }
+    })
+};
